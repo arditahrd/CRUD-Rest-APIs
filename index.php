c89e8c996f05300375c93412f8f7bdd35d1f3b93
arditahrd <arditahardi15@gmail.com>
2020-09-06 17:27:14 +0700

    final commit 2

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    <form action="http://localhost:8080/api/malay" method="post">
+        <label for="lat">Latitude</label>
+        <input type="number" name="lat" id="" step="0.00001"> <br>
+
+        <label for="long">Longitude</label>
+        <input type="number" name="long" id="" step="0.00001"> <br>
+    
+        <label for="cases">Apakah di wilayah anda terdapat?</label>
+        <select name="cases" id="">
+            <option value="0">Tidak</option>
+            <option value="1">Iya</option>
+            <option value="null">Mungkin</option>
+        </select>
+        <br>
+        <input type="submit" value="Insert">
+        <input type="reset" value="Reset">
+    </form>
+</body>
+</html>
---
+++ insert.php
@@ -0,0 +1,7 @@
+<?php
+
+$lat = $_POST['lat'];
+$long = $_POST['long'];
+$covid = $_POST['covid'];
+
+echo "$lat $long $covid";
